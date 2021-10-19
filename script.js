e03280ab94333574165cf6f6610a09c0dbecf9f4
Daniel <r20c9d00@gmail.com>
2021-10-19 21:50:29 +0200

    .

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const title = document.querySelector(".title");
-
+const produktBtn = document.querySelector(".produkt-btn");
+let btn_active = false;
 window.addEventListener("load", () => {
   title.classList.add("loaded");
+});
+
+produktBtn.addEventListener("click", () => {
+  if (!btn_active) {
+    produktBtn.innerText = "Back";
+    btn_active = true;
+  } else {
+    produktBtn.innerText = "Details";
+    btn_active = false;
+  }
 });
